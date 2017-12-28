b8de53405ca3e82331d6f5dc20c000da5ec70eea
Anthony Salgado <anthonyasalgado@gmail.com>
2017-12-27 16:27:36 -0800

    numbers with d occurences of digit d.js

--- JavaScript/7-kyu/numbersWithDOccurencesOfDigitD.js
+++ JavaScript/7-kyu/numbersWithDOccurencesOfDigitD.js
@@ -1,7 +1,19 @@
 function isDd(n) {
-    for(let i=1; i < 10; i += 1){
-        
+    for(let i=1;i<10;i++){
+        var rgxp = new RegExp(i, "g")
+        if((n.toString().match(rgxp) || []).length == i){
+            console.log(i)
+            return true
+        }
     }
-  }
+    return false
+}
+  
 
 console.log(isDd(664444309))
+console.log(isDd(122))
+console.log(isDd(30313))
+console.log(isDd(5023011))
+console.log(isDd(9081231))
+
+
