642937d21beeb0fba321bacf3806fae3b48ac1dd
Anthony Salgado <anthonyasalgado@gmail.com>
2018-01-01 16:17:49 -0800

    replaceEveryNth.js

--- JavaScript/7-kyu/replaceEveryNth.js
+++ JavaScript/7-kyu/replaceEveryNth.js
@@ -1,3 +1,35 @@
+// Write a method, that replaces every nth char oldValue with char newValue.
+// Method:
+
+// replaceNth(text, n, oldValue, newValue)
+// Example:
+
+// n:         2
+// oldValue: 'a'
+// newValue: 'o'
+// "Vader said: No, I am your father!" -> "Vader soid: No, I am your fother!"
+//   1     2          3        4       -> 2nd and 4th occurence are replaced
+
+// Your method has to be case sensitive!
+// As you can see in the example: The first changed is the 2nd 'a'. So the start is always at the nth suitable char and not at the first!
+// If n is 0 or negative or if it is larger than the count of the oldValue, return the original text without a change. 
+// The text and the chars will never be null.
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+// I have created other katas. Have a look if you like coding and challenges.
+
 function replaceNth(text, n, oldValue, newValue) {
-    
+    if(n <= 0){return text}
+    var arr = text.split("")
+    var count = 0
+    for(let i =0; i < text.length; i += 1){
+        if(arr[i] == oldValue){
+            count++
+            if(count % n == 0) {
+                arr[i] = newValue
+            }
+        }
+    }
+    return arr.join("")
   }
+
+console.log(replaceNth("Vader said: No, I am your father!", 2, 'a', 'o'))
